7870a1d1e7f6773b19ed1c55d9379bff1526e886
李博文 <5398490+sulbw@user.noreply.gitee.com>
2026-04-23 20:33:42 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,9 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-function getBasePath() {
-  if (!process.env.GITHUB_ACTIONS) return '/'
-  const repository = process.env.GITHUB_REPOSITORY || ''
-  const repoName = repository.split('/')[1] || ''
-  if (!repoName) return '/'
-  if (repoName.endsWith('.github.io')) return '/'
-  return `/${repoName}/`
-}
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/ditudadiangongneng/',
   plugins: [
     vue(),
     vueJsx(),
